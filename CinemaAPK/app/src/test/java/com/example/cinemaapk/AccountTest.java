2da7219b86ed5b0b5e61b7f359aa061462dfed1f
Gulyás Péter <gulyaspeter20@gmail.com>
2020-04-05 17:23:31 +0200

    AccountBuilder létrehozása

---
+++ CinemaAPK/app/src/test/java/com/example/cinemaapk/AccountTest.java
@@ -0,0 +1,11 @@
+package com.example.cinemaapk;
+
+public class AccountTest {
+
+    public static class AccountBuilder{
+        private String userName;
+        private String email;
+        private String password;
+
+    }
+}
