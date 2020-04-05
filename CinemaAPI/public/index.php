bf21e5acf7cb3b898f0b69f14e4f642223ca3516
Gulyás Péter <gulyaspeter20@gmail.com>
2020-04-05 21:39:23 +0200

    Create metódus létrehozása

--- CinemaAPI/public/index.php
+++ CinemaAPI/public/index.php
@@ -13,19 +13,69 @@ $app = new \Slim\App;
     method: POST
     */
 $app->post('/createuser', function(Request $request, Response $response){
+    if(!haveEmptyParameters(array('email', 'password', 'name', 'access'))){
 
+        $request_data = $_REQUEST;
+
+        $email = $request_data['email'];
+        $password = $request_data['password'];
+        $name = $request_data['name'];
+        $access = $request_data['access'];
+
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $db = new DBOperation;
+    
+        $result = $db->createUser($email, $hash_password, $name, $school);
+
+        if($result == USER_CREATED){
+            $message = array();
+            $message['error'] = false;
+            $message['message'] = 'User created successfully!';
+
+            $response->write(json_encode($message));
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(201);
+        }else if ($result == USER_FAILURE){
+            $message = array();
+            $message['error'] = true;
+            $message['message'] = 'Error!';
+
+            $response->write(json_encode($message));
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(422);
+        }else if ($result == USER_EXISTS){
+            $message = array();
+            $message['error'] = true;
+            $message['message'] = 'User already exists!';
+
+            $response->write(json_encode($message));
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(422);
+        }
+    }
 });
 
 function haveEmptyParameters($required_params, $response){
     $error = false;
     $error_params = '';
+    $required_params = $_REQUEST;
 
-    $request_params = $_REQUEST;
-
-    foreach($request_params as $param){
-        if(!isset($request_params[$param] || strlen($request_params[$param]) <= 0){
-
+    foreach($required_params as $param){
+        if(!isset($required_params[$param])|| strlen($request_params[$param]) <= 0){
+            $error = true;
+            $error_params .= $param . ', ';
         }
     }
+    if($error){
+        $error_detail = array();
+        $error_detail['error'] = true;
+        $error_detail['message'] = 'Required parameters' . substr($error_params, 0, -2) . ' are missing or empty';
+        $response->write(json_encode($error_detail));
+    }
+    return $error;
 }
 $app->run();
