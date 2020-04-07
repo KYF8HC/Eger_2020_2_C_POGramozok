4f888c79c714606a6e47fd3f5151c71de4c4d4e4
Gulyás Péter <gulyaspeter20@gmail.com>
2020-04-07 18:42:24 +0200

    Get és Get all metódus

---
+++ CinemaAPI/includes/DBOperations.php
@@ -0,0 +1,120 @@
+<?php 
+
+    class DbOperations{
+
+        private $con; 
+
+        function __construct(){
+            require_once dirname(__FILE__) . '/DbConnect.php';
+            $db = new DbConnect; 
+            $this->con = $db->connect(); 
+        }
+
+        public function createUser($email, $password, $name, $access){
+           if(!$this->isEmailExist($email)){
+                $stmt = $this->con->prepare("INSERT INTO users (email, password, name, access) VALUES (?, ?, ?, ?)");
+                $stmt->bind_param("sssi", $email, $password, $name, $access);
+                if($stmt->execute()){
+                    return USER_CREATED; 
+                }else{
+                    return USER_FAILURE;
+                }
+           }
+           return USER_EXISTS; 
+        }
+
+        public function userLogin($email, $password){
+            if($this->isEmailExist($email)){
+                $hashed_password = $this->getUsersPasswordByEmail($email); 
+                if(password_verify($password, $hashed_password)){
+                    return USER_AUTHENTICATED;
+                }else{
+                    return USER_PASSWORD_DO_NOT_MATCH; 
+                }
+            }else{
+                return USER_NOT_FOUND; 
+            }
+        }
+
+        private function getUsersPasswordByEmail($email){
+            $stmt = $this->con->prepare("SELECT password FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute(); 
+            $stmt->bind_result($password);
+            $stmt->fetch(); 
+            return $password; 
+        }
+
+        public function getAllUsers(){
+            $stmt = $this->con->prepare("SELECT id, email, name, access FROM users;");
+            $stmt->execute(); 
+            $stmt->bind_result($id, $email, $name, $access);
+            $users = array(); 
+            while($stmt->fetch()){ 
+                $user = array(); 
+                $user['id'] = $id; 
+                $user['email']=$email; 
+                $user['name'] = $name; 
+                $user['access'] = $access; 
+                array_push($users, $user);
+            }             
+            return $users; 
+        }
+
+        public function getUserByEmail($email){
+            $stmt = $this->con->prepare("SELECT id, email, name, access FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute(); 
+            $stmt->bind_result($id, $email, $name, $access);
+            $stmt->fetch(); 
+            $user = array(); 
+            $user['id'] = $id; 
+            $user['email']=$email; 
+            $user['name'] = $name; 
+            $user['access'] = $access; 
+            return $user; 
+        }
+
+        public function updateUser($email, $name, $access, $id){
+            $stmt = $this->con->prepare("UPDATE users SET email = ?, name = ?, access = ? WHERE id = ?");
+            $stmt->bind_param("ssii", $email, $name, $access, $id);
+            if($stmt->execute())
+                return true; 
+            return false; 
+        }
+
+        public function updatePassword($currentpassword, $newpassword, $email){
+            $hashed_password = $this->getUsersPasswordByEmail($email);
+            
+            if(password_verify($currentpassword, $hashed_password)){
+                
+                $hash_password = password_hash($newpassword, PASSWORD_DEFAULT);
+                $stmt = $this->con->prepare("UPDATE users SET password = ? WHERE email = ?");
+                $stmt->bind_param("ss",$hash_password, $email);
+
+                if($stmt->execute())
+                    return PASSWORD_CHANGED;
+                return PASSWORD_NOT_CHANGED;
+
+            }else{
+                return PASSWORD_DO_NOT_MATCH; 
+            }
+        }
+
+        public function deleteUser($id){
+            $stmt = $this->con->prepare("DELETE FROM users WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            if($stmt->execute())
+                return true; 
+            return false; 
+        }
+
+
+        private function isEmailExist($email){
+            $stmt = $this->con->prepare("SELECT id FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute(); 
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0;  
+        }
+    }
--- CinemaAPI/public/index.php
+++ CinemaAPI/public/index.php
@@ -1,76 +1,160 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
-require __DIR__ . '/../vendor/autoload.php';
-require '../includes/DBConnect.php';
 
-$app = new \Slim\App;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-/*
+require '../vendor/autoload.php';
+
+require '../includes/DbOperations.php';
+
+$app = new \Slim\App([
+    'settings'=>[
+        'displayErrorDetails'=>true
+    ]
+]);
+
+
+/* 
     endpoint: createuser
-    parameters: email, password, name, school
+    parameters: email, password, name, access
     method: POST
-    */
+*/
+$app->post('/createuser', function(Request $request, Response $response){
 
+    if(!haveEmptyParameters(array('email', 'password', 'name', 'access'), $request, $response)){
 
-    $app->post('/createuser', function(Request $request, Response $response){
+        $request_data = $request->getParsedBody(); 
 
-        if(!haveEmptyParameters(array('email', 'password', 'name', 'school'), $request, $response)){
-    
-            $request_data = $request->getParsedBody(); 
-    
-            $email = $request_data['email'];
-            $password = $request_data['password'];
-            $name = $request_data['name'];
-            $school = $request_data['school']; 
-    
-            $hash_password = password_hash($password, PASSWORD_DEFAULT);
-    
-            $db = new DbOperations; 
-    
-            $result = $db->createUser($email, $hash_password, $name, $school);
-            
-            if($result == USER_CREATED){
-    
-                $message = array(); 
-                $message['error'] = false; 
-                $message['message'] = 'User created successfully';
-    
-                $response->write(json_encode($message));
-    
-                return $response
-                            ->withHeader('Content-type', 'application/json')
-                            ->withStatus(201);
-    
-            }else if($result == USER_FAILURE){
-    
-                $message = array(); 
-                $message['error'] = true; 
-                $message['message'] = 'Some error occurred';
-    
-                $response->write(json_encode($message));
-    
-                return $response
-                            ->withHeader('Content-type', 'application/json')
-                            ->withStatus(422);    
-    
-            }else if($result == USER_EXISTS){
-                $message = array(); 
-                $message['error'] = true; 
-                $message['message'] = 'User Already Exists';
-    
-                $response->write(json_encode($message));
-    
-                return $response
-                            ->withHeader('Content-type', 'application/json')
-                            ->withStatus(422);    
-            }
+        $email = $request_data['email'];
+        $password = $request_data['password'];
+        $name = $request_data['name'];
+        $school = $request_data['access']; 
+
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $db = new DbOperations; 
+
+        $result = $db->createUser($email, $hash_password, $name, $school);
+        
+        if($result == USER_CREATED){
+
+            $message = array(); 
+            $message['error'] = false; 
+            $message['message'] = 'User created successfully';
+
+            $response->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(201);
+
+        }else if($result == USER_FAILURE){
+
+            $message = array(); 
+            $message['error'] = true; 
+            $message['message'] = 'Some error occurred';
+
+            $response->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);    
+
+        }else if($result == USER_EXISTS){
+            $message = array(); 
+            $message['error'] = true; 
+            $message['message'] = 'User Already Exists';
+
+            $response->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);    
         }
-        return $response
-            ->withHeader('Content-type', 'application/json')
-            ->withStatus(422);    
-    });
+    }
+    return $response
+        ->withHeader('Content-type', 'application/json')
+        ->withStatus(422);    
+});
+
+$app->post('/userlogin', function(Request $request, Response $response){
+
+    if(!haveEmptyParameters(array('email', 'password'), $request, $response)){
+        $request_data = $request->getParsedBody(); 
+
+        $email = $request_data['email'];
+        $password = $request_data['password'];
+        
+        $db = new DbOperations; 
+
+        $result = $db->userLogin($email, $password);
+
+        if($result == USER_AUTHENTICATED){
+            
+            $user = $db->getUserByEmail($email);
+            $response_data = array();
+
+            $response_data['error']=false; 
+            $response_data['message'] = 'Login Successful';
+            $response_data['user']=$user; 
+
+            $response->write(json_encode($response_data));
+
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(200);    
+
+        }else if($result == USER_NOT_FOUND){
+            $response_data = array();
+
+            $response_data['error']=true; 
+            $response_data['message'] = 'User not exist';
+
+            $response->write(json_encode($response_data));
+
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(200);    
+
+        }else if($result == USER_PASSWORD_DO_NOT_MATCH){
+            $response_data = array();
+
+            $response_data['error']=true; 
+            $response_data['message'] = 'Invalid credential';
+
+            $response->write(json_encode($response_data));
+
+            return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(200);  
+        }
+    }
+
+    return $response
+        ->withHeader('Content-type', 'application/json')
+        ->withStatus(422);    
+});
+
+$app->get('/allusers', function(Request $request, Response $response){
+
+    $db = new DbOperations; 
+
+    $users = $db->getAllUsers();
+
+    $response_data = array();
+
+    $response_data['error'] = false; 
+    $response_data['users'] = $users; 
+
+    $response->write(json_encode($response_data));
+
+    return $response
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);  
+
+});
+
 
 function haveEmptyParameters($required_params, $request, $response){
     $error = false; 
@@ -92,4 +176,5 @@ function haveEmptyParameters($required_params, $request, $response){
     }
     return $error; 
 }
+
 $app->run();
