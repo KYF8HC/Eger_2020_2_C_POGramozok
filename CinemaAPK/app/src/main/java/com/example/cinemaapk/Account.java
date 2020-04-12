87265b7cd64bf6cb0bdd2ac9d3ce3879e911ae5c
Gulyás Péter <gulyaspeter20@gmail.com>
2020-04-12 17:57:10 +0200

    Regisztráció(kliens oldalon)

--- CinemaAPK/app/src/main/java/com/example/cinemaapk/Account.java
+++
@@ -1,4 +0,0 @@
-package com.example.cinemaapk;
-
-public class Account {
-}
---
+++ CinemaApiAS/app/src/main/java/com/example/cinemaapias/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.cinemaapias;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextEmail, editTextPassword, editTextName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }
+
+
+    private void userSignUp() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8){
+            editTextPassword.setError("The password must be at least 8 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (name.isEmpty()){
+            editTextName.setError("Email is required!");
+            editTextName.requestFocus();
+            return;
+        }
+
+        Call<ResponseBody> call = Client.getInstance().getApi()
+                .createUser(email, password, name, 2);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try{
+                    String s = response.body().string();
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonSignUp:
+                userSignUp();
+                break;
+            case R.id.textViewLogin:
+
+                break;
+        }
+    }
+
+}
