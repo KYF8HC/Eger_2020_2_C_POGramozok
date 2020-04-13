ba1f6f7d2e8ef8f6afe118780edc4573ac30c888
Gulyás Péter <gulyaspeter20@gmail.com>
2020-04-13 18:28:10 +0200

    Login rendszer

--- CinemaApiAS/app/src/main/java/com/example/cinemaapias/activities/MainActivity.java
+++ CinemaApiAS/app/src/main/java/com/example/cinemaapias/activities/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.cinemaapias;
+package com.example.cinemaapias.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -8,12 +9,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
+import com.example.cinemaapias.R;
+import com.example.cinemaapias.api.Client;
+import com.example.cinemaapias.models.DefaultResponse;
+import com.example.cinemaapias.storage.SharedPrefManager;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,71 +39,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(SharedPrefManager.getInstance(this).isLoggedIn()){
+            Intent intent = new Intent(this, ProfileActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
     private void userSignUp() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
         String name = editTextName.getText().toString().trim();
 
-        if (email.isEmpty()){
-            editTextEmail.setError("Email is required!");
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
             editTextEmail.requestFocus();
             return;
         }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextEmail.setError("Please enter a valid email!");
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Enter a valid email");
             editTextEmail.requestFocus();
             return;
         }
-
-        if (password.isEmpty()){
-            editTextPassword.setError("Password is required!");
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password required");
             editTextPassword.requestFocus();
             return;
         }
-
-        if (password.length() < 8){
-            editTextPassword.setError("The password must be at least 8 characters!");
+        if (password.length() < 6) {
+            editTextPassword.setError("Password should be at least 6 character long");
             editTextPassword.requestFocus();
             return;
         }
-
-        if (name.isEmpty()){
-            editTextName.setError("Email is required!");
+        if (name.isEmpty()) {
+            editTextName.setError("Name required");
             editTextName.requestFocus();
             return;
         }
-
-        Call<ResponseBody> call = Client.getInstance().getApi()
+        Call<DefaultResponse> call = Client
+                .getInstance()
+                .getApi()
                 .createUser(email, password, name, 2);
-        call.enqueue(new Callback<ResponseBody>() {
+        call.enqueue(new Callback<DefaultResponse>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                try{
-                    String s = response.body().string();
-                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                } catch (IOException e){
-                    e.printStackTrace();
+            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+                if (response.code() == 201) {
+                    DefaultResponse dr = response.body();
+                    Toast.makeText(MainActivity.this, dr.getMsg(), Toast.LENGTH_LONG).show();
+                } else if (response.code() == 422) {
+                    Toast.makeText(MainActivity.this, "User already exist", Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            public void onFailure(Call<DefaultResponse> call, Throwable t) {
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.buttonSignUp:
                 userSignUp();
                 break;
             case R.id.textViewLogin:
-
+                startActivity(new Intent(this, LoginActivity.class));
                 break;
         }
     }
-
 }
---
+++ CinemaApiAS/app/src/main/java/com/example/cinemaapias/api/API.java
@@ -0,0 +1,58 @@
+package com.example.cinemaapias.api;
+
+import com.example.cinemaapias.models.DefaultResponse;
+import com.example.cinemaapias.models.LoginResponse;
+import com.example.cinemaapias.models.UsersResponse;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface API {
+
+
+        @FormUrlEncoded
+        @POST("createuser")
+        Call<DefaultResponse> createUser(
+                @Field("email") String email,
+                @Field("password") String password,
+                @Field("name") String name,
+                @Field("access") int access
+        );
+
+        @FormUrlEncoded
+        @POST("userlogin")
+        Call<LoginResponse> userLogin(
+                @Field("email") String email,
+                @Field("password") String password
+        );
+
+        @GET("allusers")
+        Call<UsersResponse> getUsers();
+
+        @FormUrlEncoded
+        @PUT("updateuser/{id}")
+        Call<LoginResponse> updateUser(
+                @Path("id") int id,
+                @Field("email") String email,
+                @Field("name") String name,
+                @Field("access") String access
+        );
+
+        @FormUrlEncoded
+        @PUT("updatepassword")
+        Call<DefaultResponse> updatePassword(
+                @Field("currentpassword") String currentpassword,
+                @Field("newpassword") String newpassword,
+                @Field("email") String email
+        );
+
+        @DELETE("deleteuser/{id}")
+        Call<DefaultResponse> deleteUser(@Path("id") int id);
+
+    }
