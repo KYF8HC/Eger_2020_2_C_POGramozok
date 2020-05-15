d855465d296d3e49c13e687d0d4714bafebf0013
II4ELD <55428181+dpatrik0630@users.noreply.github.com>
2020-05-15 15:54:37 +0200

    import+ onCreate bővítés

--- CinemaApiAS/app/src/main/java/com/example/cinemaapias/activities/MainActivity.java
+++ CinemaApiAS/app/src/main/java/com/example/cinemaapias/activities/MainActivity.java
@@ -18,6 +18,28 @@ import com.example.cinemaapias.storage.SharedPreferenceManager;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -35,6 +57,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.buttonSignUp).setOnClickListener(this);
         findViewById(R.id.textViewLogin).setOnClickListener(this);
+
+         db = new DatabaseHelper(getApplicationContext());
+        sql = db.getWritableDatabase();
+        Toast toast = Toast.makeText(getApplicationContext(), "Confirmation Page", Toast.LENGTH_LONG);
+        Intent intent = getIntent();
+        Bundle seat_info = intent.getBundleExtra("seat_information");
+        final String movie_id = intent.getStringExtra("movie_id");
+        final String date1 = intent.getStringExtra("date");
+        final String time1 = intent.getStringExtra("time");
+        final String theatre1 = intent.getStringExtra("theatre");
+
+        ArrayList<String> seating = seat_info.getStringArrayList("seats");
+        ArrayList<String> total = seat_info.getStringArrayList("total");
+        String movie_name = null;
+        System.out.print(movie_id);
+        String seat_number = total.get(0);
+
+        System.out.println("The seat number value is" + seat_number);
+
+
+        total_price = Integer.parseInt(seat_number) * 10;
+
+        String price_final = "$" + Integer.toString(total_price);
+        Cursor movie = db.getMoviename(sql, movie_id);
+
+        while (movie.moveToNext()) {
+            movie_name = movie.getString(0);
+
+        }
+        arr.add(0, movie_name);
+        int len = seating.size();
+        arr.add(1, theatre1);
+        arr.add(2, date1);
+        arr.add(3, time1);
+        arr.add(4, price_final);
+
+        final TextView seats = (TextView) findViewById(R.id.noofseats);
+        final TextView theatre = (TextView) findViewById(R.id.theascr);
+        TextView date = (TextView) findViewById(R.id.date);
+        TextView time = (TextView) findViewById(R.id.time);
+        TextView price = (TextView) findViewById(R.id.price);
+        email = (EditText) findViewById(R.id.emailid);
+        phone = (EditText) findViewById(R.id.phone);
+        SpannableString styledString = new SpannableString(arr.get(0));
+        styledString.setSpan(new StyleSpan(Typeface.BOLD), 0, arr.get(0).length(), 0);
+
+        TextView tv = (TextView) findViewById(R.id.textView16);
+        tv.setText(styledString);
+        ImageView imageview = (ImageView) findViewById(R.id.imageView3);
+        switch (Integer.parseInt(movie_id)) {
+            case 1:
+                imageview.setImageResource(R.drawable.images);
+                break;
+            case 2:
+                imageview.setImageResource(R.drawable.mi55);
+                break;
+            case 3:
+                imageview.setImageResource(R.drawable.avengers3);
+                break;
+            case 4:
+                imageview.setImageResource(R.drawable.spiddy);
+                break;
+        }
+        seats.setText("");
+        for (int i = 0; i < len; i++) {
+            seats.setText(seats.getText() + seating.get(i));
+            if (i != len - 1) {
+                seats.setText(seats.getText() + ",");
+            }
+        }
+        theatre.setText(arr.get(1));
+        time.setText(arr.get(3));
+        date.setText(arr.get(2));
+        price.setText(arr.get(4));
+        final Bundle bundle = new Bundle();
+        bundle.putString("movie_id", movie_id);
+        bundle.putString("date", date1);
+        bundle.putBundle("seat_information", seat_info);
+        bundle.putString("time", time1);
+        bundle.putString("theatre", theatre1);
+
+        bundle.putString("price", price.getText().toString());
+        Button bktkt = (Button) findViewById(R.id.pay);
+        bktkt.setOnClickListener(new View.OnClickListener() */
+
     }
 
     @Override
@@ -119,5 +226,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(this, LoginActivity.class));
                 break;
         }
+
     }
 }
